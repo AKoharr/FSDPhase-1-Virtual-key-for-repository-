a77e2ae9c8736e45c050396946570372153ea563
AKoharr <110048608+AKoharr@users.noreply.github.com>
2022-08-04 11:44:43 +0530

    Add files via upload

---
+++ fsdJAVAPhase1/Main.java
@@ -0,0 +1,14 @@
+package fsdJAVAPhase1;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\Anu\\Desktop\\fsdSimplilearn\\proj_File";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+
+	}
+
+}
